f5f009e639d2a00d1e0188453b83be4ad7fdde5f
JiangFan <jiangfanloveen@outlook.com>
2019-06-19 23:08:16 +0800

    Site updated: 2019-06-19 23:08:15

--- TL_changyige/crawl.py
+++ TL_changyige/crawl.py
@@ -12,10 +12,12 @@ import MySQLdb
 
 menpai_dict = {"少林": 0, "明教": 1, "丐帮": 2, "武当": 3, "峨嵋": 4, "星宿": 5 ,"天龙": 6, "天山": 7, "逍遥": 8, "慕容": 9, "唐门":10, "鬼谷": 11}
 
+sex_dict = {"女": 0, "男": 1}
+
 # get the page information of all the goods in sale
-def getData(url, userAgent):
+def getData(url):
     header = {
-     'User-Agent': userAgent
+     'User-Agent': random.choice(agentHeaders)
     }
     while(True):
         html = requests.get(url, headers=header)
@@ -29,21 +31,33 @@ def getData(url, userAgent):
         name_pure = name[1: len(name)-1]
         score_equipment = item.find('b')
         price = item.find('p', class_='price').get_text()[1:]
-        id = item.find('a', class_='r-img').get('href').split("=")[1]
+        url_this = item.find('a', class_='r-img').get('href')
+        id = url_this.split("=")[1]        
         split_str = name_pure.split(" ")
-        menpai = split_str[0]
+        menpai = menpai_dict[split_str[0]]
+        sex = sex_dict[split_str[1]]
         rank = split_str[2]
         rank_pure = rank[0: len(rank)-1]
-        # chonglou = False
-        # if item.find('i', class_='icon-cl'):
-        #     chonglou = True
-        print(id, menpai_dict[menpai], rank_pure, price)
-        write_data(id, menpai_dict[menpai], rank_pure, price)
+        chonglou = False
+        if item.find('i', class_='icon-cl'):
+            chonglou = True
+        html_this = requests.get(url_this, headers=header)
+        if html_this.status_code == 200:
+            soup_this = BeautifulSoup(html_this.content, "html.parser", from_encoding='utf-8')
+            bottom = soup_this.find('fn-fix-info')
+            btm_info = bottom.find_all('span', class_='span')
+            score_equipment = btm_info[1].get_text()
+            score_diamond = btm_info[6].get_text()
+            right = soup_this.find('div', class_='h422')
+            blood = right.find('i', class_='fn-high-light').get_text()
+            wuyi_level = 100
+        print(id, sex, chonglou, price, menpai, rank_pure, score_equipment, score_diamond, blood, wuyi_level) 
+        write_data(id, sex, chonglou, price, menpai, rank_pure, score_equipment, score_diamond, blood, wuyi_level)
         # print(name, score_equipment.get_text(), price.get_text(), id, chonglou)
 
-def write_data(id, menpai, rank, price):
-    sql = "insert into goods value(%s, %s, %s, %s)" %(id, menpai, rank, price)
-    print(sql)
+def write_data(id, sex, chonglou, price, menpai, rank_pure, score_equipment, score_diamond, blood, wuyi_level):
+    sql = "insert into goods value(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)" %(id, sex, chonglou, price, menpai, rank_pure, score_equipment, score_diamond, blood, wuyi_level)
+    # print(sql)
     try:
         cursor = db.cursor()
         cursor.execute(sql)
@@ -68,8 +82,19 @@ db = MySQLdb.connect('localhost', 'root', 'hc7783au', 'tl')
 agentHeaders = LoadUserAgents("user_agents.txt")
 t1 = datetime.now()
 raw_url = "http://tl.cyg.changyou.com/goods/selling?&page_num="
-for i in range(1, 100):
+for i in range(1, 10):
     getData(raw_url+str(i), random.choice(agentHeaders))
 t2 = datetime.now()
 print("time=", (t2-t1).seconds)
 db.close()
+
+# create table goods (id varchar(20) primary key,
+# sex int not null,
+# chonglou int not null,
+# price int not null,
+# menpai int not null,
+# rank int not null,
+# score_equipment int not null,
+# score_diamond int not null,
+# blood int not null,
+# wuyi_level int not null);
