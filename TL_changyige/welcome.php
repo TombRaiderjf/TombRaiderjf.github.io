abdca3eb53a036c014475c41619f7b928ec9cb43
JiangFan <jiangfanloveen@outlook.com>
2019-06-22 23:31:09 +0800

    Site updated: 2019-06-22 23:31:08

--- TL_changyige/welcome.php
+++ TL_changyige/welcome.php
@@ -6,31 +6,34 @@ $password = "hc7783au";
 $dbname = "tl";
 
 
-$array['sex'] = array();
-$array['chonglou'] = $_POST['chonglou'];
-$array['sex'] = $_POST['sex'];
-die(json_encode($array));
+// $array['sex'] = array();
+// $array['chonglou'] = $_POST['chonglou'];
+// $array['sex'] = $_POST['sex'];
+// die(json_encode($array));
 
 $connect = new mysqli($servername, $username, $password, $dbname);
 
-if($connect->connect_error)
-{
-    echo "连接失败！";
-    die("连接失败：". $connect->connect_error);
-}
-else{
-    echo "连接成功！";
-}
+// if($connect->connect_error)
+// {
+//     echo "连接失败！";
+//     die("连接失败：". $connect->connect_error);
+// }
+// else{
+//     echo "连接成功！";
+// }
 
 $sql = "SELECT id, score_equipment FROM goods";
 $result = $connect->query($sql);
 
 
-
 if ($result->num_rows > 0) {
     // 输出数据
+    
     while($row = $result->fetch_assoc()) {
-        echo "列名1: " . $row["id"]. " 列名2: " . $row["score_equipment"]."<br>";
+        // echo "列名1: " . $row["id"]. " 列名2: " . $row["score_equipment"]."<br>";
+        $data = array('id'=>$row["id"], 'score_equipment'=>$row["score_equipment"]);
+        die(json_encode($data));
+        break;
     }
 } else {
     echo "0 结果";
