1514f1f7e128330b8bbd2a04f199c1264f4d3d99
JiangFan <jiangfanloveen@outlook.com>
2019-06-22 12:44:07 +0800

    Site updated: 2019-06-22 12:44:07

--- TL_changyige/welcome.php
+++ TL_changyige/welcome.php
@@ -5,25 +5,25 @@ $username = "root";
 $password = "hc7783au";
 $dbname = "tl";
 
-$connect = new mysqli($servername, $username, $password, $dbname);
+// $connect = new mysqli($servername, $username, $password, $dbname);
 
-if($connect->connect_error)
-{
-    die("连接失败：". $connect->connect_error);
-}
+// if($connect->connect_error)
+// {
+//     die("连接失败：". $connect->connect_error);
+// }
 
-$sql = "SELECT id, score_equipment FROM goods";
-$result = $conn->query($sql);
+// $sql = "SELECT id, score_equipment FROM goods";
+// $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-    // 输出数据
-    while($row = $result->fetch_assoc()) {
-        echo "列名1: " . $row["列名1"]. " 列名2: " . $row["列名2"]."<br>";
-    }
-} else {
-    echo "0 结果";
-}
-$connect->close();
+// if ($result->num_rows > 0) {
+//     // 输出数据
+//     while($row = $result->fetch_assoc()) {
+//         echo "列名1: " . $row["列名1"]. " 列名2: " . $row["列名2"]."<br>";
+//     }
+// } else {
+//     echo "0 结果";
+// }
+// $connect->close();
 
 
 $array['sex'] = array();
