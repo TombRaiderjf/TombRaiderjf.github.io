eece5a38ab19328444e7bcde73534ca3900f9f13
JiangFan <jiangfanloveen@outlook.com>
2019-06-22 13:16:02 +0800

    Site updated: 2019-06-22 13:16:02

--- TL_changyige/index.js
+++ TL_changyige/index.js
@@ -2,7 +2,22 @@ $(document).ready(function(){
     $("#check").click(function(){
         var sex = $('input:radio[name="sex"]:checked').val();
         var chonglou = $('input:radio[name="chonglou"]:checked').val();
-        var postData = {"sex": sex, "chonglou": chonglou};
+        var price = $('input:radio[name="price"]:checked').val();
+        var rank = $('input:radio[name="rank"]:checked').val();
+        var score_equipment = $('input:radio[name="score_equipment"]:checked').val();
+        var score_diamond = $('input:radio[name="score_diamond"]:checked').val();
+        var blood = $('input:radio[name="blood"]:checked').val();
+        var wuyi_level = 100;
+        var postData = {
+            "sex": sex, 
+            "chonglou": chonglou, 
+            "price": price, 
+            "rank": rank, 
+            "score_equipment": score_equipment, 
+            "score_diamond": score_diamond,
+            "blood": blood,
+            "wuyi_level": wuyi_level
+        };
         console.log($('input:radio:checked').val());
         $.ajax({
             url:'http://47.102.140.114/TL_changyige/welcome.php',//目的php文件
