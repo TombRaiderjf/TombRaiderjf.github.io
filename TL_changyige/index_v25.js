f66235616fcaaa63633a0c70012925cb38b344d1
JiangFan <jiangfanloveen@outlook.com>
2019-06-30 00:20:23 +0800

    Site updated: 2019-06-30 00:20:23

--- TL_changyige/index_v25.js
+++ TL_changyige/index_v25.js
@@ -75,7 +75,7 @@ $(document).ready(function(){
                     res = "";
                     for(var i=0; i<value.length; i++)
                     {
-                        res += ("<img src='image/坐骑/" + ride_dict[value[i]] + ".jpg' width=19px title='" + ride_dict[value[i]] + "'>");
+                        res += ("<img src='image/坐骑/" + ride_dict[value[i]] + ".jpg' width=19px>");
                     }
                     return res;
                 }
