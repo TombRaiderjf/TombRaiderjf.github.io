fe55563ea79063bd24248d5b09fb97a94e28afcd
JiangFan <jiangfanloveen@outlook.com>
2019-06-30 22:53:32 +0800

    Site updated: 2019-06-30 22:53:31

--- TL_changyige/updateData_v2.py
+++ TL_changyige/updateData_v2.py
@@ -26,7 +26,7 @@ sex_dict = {"女": 0, "男": 1}
 def updateId():
     ids = {}
     cl = {}
-    for j in range(1, 100):
+    for j in range(1, 500):
         updateUrl(raw_url + str(j), ids, cl)
     print("total data ", len(ids))
     return ids, cl
