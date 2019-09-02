c99b56f088318ed17e7a188bb5a53cd9cfb24f84
JiangFan <jiangfanloveen@outlook.com>
2019-09-02 23:10:46 +0800

    Site updated: 2019-09-02 23:10:46

--- TL_changyige/addInfo.php
+++ TL_changyige/addInfo.php
@@ -1,12 +1,13 @@
 <?php
 // 处理提交的发布信息请求
 header('Access-Control-Allow-Origin:http://tombraiderjf.com');
-$output = exec("python addInfo.py {$_POST['id']} {$_POST['method']} {$_POST['contact']}", $out, $res);
-if($res == 0){
-    echo 'success';
-}
-else{
-    echo 'error';
-}
+echo  '-1';
+// $output = exec("python addInfo.py {$_POST['id']} {$_POST['method']} {$_POST['contact']}", $out, $res);
+// if($res == 0){
+//     echo 'success';
+// }
+// else{
+//     echo 'error';
+// }
 
 ?>
