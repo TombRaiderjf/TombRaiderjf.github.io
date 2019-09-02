7f7c89f048ab5628da20c7b7808b8946072bd0d7
JiangFan <jiangfanloveen@outlook.com>
2019-09-02 23:24:48 +0800

    Site updated: 2019-09-02 23:24:47

--- TL_changyige/addInfo.php
+++ TL_changyige/addInfo.php
@@ -6,7 +6,7 @@ $id = $_POST['id'];
 $method = $_POST['method'];
 $contact = $_POST['contact'];
 // $output = system("python addInfo.py {$id} {$method} {$contact}");
-$output = system("python addInfo.py 201908201331248636 1 21314542");
+$output = shell_exec("python addInfo.py 201908201331248636 1 21314542");
 echo $output;
 $array = explode(',', $output);
 echo $array;
