aa0b6741b55914457ef44f4b6a8a478b4397c356
JiangFan <jiangfanloveen@outlook.com>
2019-09-02 23:34:31 +0800

    Site updated: 2019-09-02 23:34:31

--- TL_changyige/addInfo.php
+++ TL_changyige/addInfo.php
@@ -5,12 +5,12 @@ header('Access-Control-Allow-Origin:http://tombraiderjf.com');
 $id = $_POST['id'];
 $method = $_POST['method'];
 $contact = $_POST['contact'];
-echo $id;
+// echo $id;
 // $output = system("python addInfo.py {$id} {$method} {$contact}");
 $output = shell_exec("python addInfo.py 201908201331248636 1 21314542");
 echo "-1";
-echo $output;
-$array = explode(',', $output);
-echo $array;
+// echo $output;
+// $array = explode(',', $output);
+// echo $array;
 
 ?>
